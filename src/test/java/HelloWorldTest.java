9fc7264ae90ec7f81ac23644af81690d30fa90cc
Weber Lin <wayne2347@gmail.com>
2020-12-24 14:08:03 +0800

    Add resource and test class for mvn course

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,10 @@
+
+public class HelloWorldTest {
+	public void testHello() {
+		System.out.println("TEST Hello...");
+	}
+	
+	public void testWorld() {
+		System.out.println("TEST - World!!!");
+	}
+}
